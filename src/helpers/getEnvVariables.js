1d5051e7fcff846cb28cc7165358304beeee76db
Stefano Garbocci <stefgarbocci@gmail.com>
2023-02-13 15:23:16 -0300

    Primer commit

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -3,14 +3,11 @@
 export const getEnvVariables = () => {  
 
     // import.meta.env;
-    // const jota = JSON.stringify(import.meta.env);
     const {VITE_API_URL} =  import.meta.env;
-    // import.meta.env = JSON.stringify(import.meta.env);
 
     return {
         VITE_API_URL
-        // ...import.meta.env
-        // jota
+
     }
     
 }
